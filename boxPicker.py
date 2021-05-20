45d9c57cce62e322fbe9fb451b11fc3b70c6f202
Ian Joughin <ian@apl.washington.edu>
2021-05-20 16:34:21 -0700

    Fixed boxPicker

--- boxPicker.py
+++ boxPicker.py
@@ -70,6 +70,7 @@ class boxPicker():
         da = rioxarray.open_rasterio(self.mapUrl, overview_level=3, parse_coordinates=True,
                                      chunks=dict(band=1, y=512, x=512), #dict(band=1, y="auto", x=-1),
                                      masked=False).squeeze('band')
+        da
         da = da.rename(dict(x='easting', y='northing'))
         img = da.hvplot.image(rasterize=True, cmap='gray',
                               aspect='equal', frame_width=400,
